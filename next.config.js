3902b218dbc78848ce00bd387d4827b47bf0b223
dev-bitstech <develop@bitstech.solutions>
2025-08-14 19:25:05 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',              // << gera site estático
+  images: { unoptimized: true }, // evita pipeline de imagens no build
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 module.exports = nextConfig;
